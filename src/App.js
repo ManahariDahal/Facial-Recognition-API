c411df1e7f1e84e057481bcda50c9692e9490426
ManahariDahal <manahari.dahal1@gmail.com>
2020-08-19 17:15:13 -0600

    Homepage updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import Navigation from './Components/Navigation/Navigation.js';
 import Logo from './Components/Logo/Logo.js'
-// import logo from './logo.svg';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './Components/Rank/Rank.js';
 import './App.css';
+import Particles from 'react-particles-js';
 
-class App extends Component {
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 600
+      }
+    }
+  }
+}
+
+
+class App extends Component 
+{
   render() {
     return (
       <div className="App">
+      <Particles className='particles'
+              params={particlesOptions}
+            />
         <Navigation />
         <Logo />
-        { /* <ImageLinkForm />
-        <FaceRecognition /> */}
+        <Rank />
+        <ImageLinkForm />
+        { /* <FaceRecognition /> */}
       </div>
     );
   }
