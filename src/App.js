1ee7a006d49efb8dc85861a8012f1df5442bc357
ManahariDahal <manahari.dahal1@gmail.com>
2020-08-21 18:26:48 -0600

    front-end part finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import Navigation from './Components/Navigation/Navigation.js';
-import Logo from './Components/Logo/Logo.js'
+import Logo from './Components/Logo/Logo.js';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm.js';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition.js';
 import Rank from './Components/Rank/Rank.js';
 import './App.css';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import Signin from './Components/Signin/Signin.js';
+import Register from './Components/Register/Register.js';
+
+
+const app = new Clarifai.App({
+  apiKey: '2007d9744e3244a1a6ed49b941813b3b'
+});
+
 
 const particlesOptions = {
   particles: {
@@ -19,19 +29,92 @@ const particlesOptions = {
 }
 
 
-class App extends Component 
-{
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+     const image = document.getElementById('inputImage');
+     const width = Number(image.width);
+     const height = Number(image.height);
+     return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height)
+     }
+  }
+
+
+
+  displayFaceBox = (box) => {
+    // console.log(box);
+      this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+
+  onButtonSubmit = () => {
+    // console.log('click');
+    this.setState({imageUrl: this.state.input});
+      app.models
+        .predict(Clarifai.FACE_DETECT_MODEL,
+                this.state.input)
+        .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+        .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) =>{
+    if (route === 'signout'){
+        this.setState({isSignedIn: false})
+    }
+
+    else if (route === 'home') {
+        this.setState({isSignedIn: true})
+    }
+    this.setState({route: route})  
+  }
+
+
   render() {
+    const {isSignedIn, imageUrl, route, box} = this.state;
     return (
       <div className="App">
       <Particles className='particles'
               params={particlesOptions}
             />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm />
-        { /* <FaceRecognition /> */}
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home'   
+          ?    <div>
+                <Logo />
+                <Rank />
+                <ImageLinkForm 
+                    onInputChange={this.onInputChange} 
+                    onButtonSubmit={this.onButtonSubmit} />
+                <FaceRecognition box={box} imageUrl={imageUrl}/>
+            </div>
+
+          : (
+            route === 'signin'
+            ? <Signin onRouteChange={this.onRouteChange} />
+            : <Register onRouteChange={this.onRouteChange} />
+          )
+
+          
+           
+        }
       </div>
     );
   }
--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 import './Logo.css';
-import computer from './computer.png';
+import mainlogo from './mainlogo.png';
 
 const Logo = () => {
 	return (
 		<div className="ma4 mt0">
 			<Tilt className="Tilt br2 shadow-2" options={{ max : 45 }} style={{ height: 150, width: 150 }} >
 			 	<div className="Tilt-inner pa3">
-			 		<img style={{paddingTop: '5px'}}src={computer} alt='logo' />
+			 		<img style={{paddingTop: '5px'}}src={mainlogo} alt='logo' />
 			 	</div>
 			</Tilt>
 		</div>
